3b9e1ad53d920b7a957c8f63cdff188dff999535
ninoy rahman <ninoyrahman@gmail.com>
2025-11-20 12:53:04 +0100

    added option to change view angle in 3d plot

--- FEM_plot.py
+++ FEM_plot.py
@@ -5,7 +5,7 @@ import matplotlib as mpl
 import matplotlib.cm as cm
 import numpy as np
 
-def plot3d(ax, data, varmin, varmax, x, y, label=r'u', cmap=cm.coolwarm):
+def plot3d(ax, data, varmin, varmax, x, y, label=r'u', cmap=cm.coolwarm, azim=130):
     var = np.array(data, copy=True)
     lrange = np.linspace(varmin, varmax, 11)
     var[var < varmin] = varmin
@@ -13,7 +13,7 @@ def plot3d(ax, data, varmin, varmax, x, y, label=r'u', cmap=cm.coolwarm):
 
     # print(lrange)
     
-    ax.view_init(elev=20, azim=130, roll=0)
+    ax.view_init(elev=20, azim=azim, roll=0)
     
     surf = ax.plot_trisurf(x, y, var,
                            linewidth=0.2, antialiased=True, cmap=cmap, label="FE")
@@ -22,6 +22,7 @@ def plot3d(ax, data, varmin, varmax, x, y, label=r'u', cmap=cm.coolwarm):
     ax.set_ylim([y.min(), y.max()])
     ax.set_xlabel(r'$x$')
     ax.set_ylabel(r'$y$')
+    # ax.set_aspect('equal')
     
     cbar = plt.colorbar(surf, ax=ax, orientation="horizontal", pad=0.02, ticks=lrange)
     cbar.ax.set_xlabel(label)
