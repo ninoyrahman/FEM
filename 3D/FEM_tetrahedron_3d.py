bug fix for heat equation rhs calculation

3D/FEM_tetrahedron_3d.py
@@ -167,7 +167,7 @@ class GaussianQuadrature:
 
 
     # Calculate the numerical integration for each node
-    def calculate(self, _f, p1, p2, p3, p4, sm=True):
+    def calculate(self, _f, p1, p2, p3, p4, heat=False):
         """
         Parameters
         ----------
@@ -181,15 +181,15 @@ class GaussianQuadrature:
         # Get the global (x,y) coordinates at the weighted points
         xys = [self.tri_element.get_xy(wp[0], wp[1], wp[2], p1, p2, p3, p4) for wp in self.wps]
 
-        if not sm:
+        if heat:
             return np.array([
-            sum([w * _f(xy[0], xy[1], xy[2])[0] * self.tri_element.N1(
+            sum([w * _f(xy[0], xy[1], xy[2]) * self.tri_element.N1(
                 wp[0], wp[1], wp[2]) for w, wp, xy in zip(self.ws, self.wps, xys)]),
-            sum([w * _f(xy[0], xy[1], xy[2])[0] * self.tri_element.N2(
+            sum([w * _f(xy[0], xy[1], xy[2]) * self.tri_element.N2(
                 wp[0], wp[1], wp[2]) for w, wp, xy in zip(self.ws, self.wps, xys)]),
-            sum([w * _f(xy[0], xy[1], xy[2])[0] * self.tri_element.N3(
+            sum([w * _f(xy[0], xy[1], xy[2]) * self.tri_element.N3(
                 wp[0], wp[1], wp[2]) for w, wp, xy in zip(self.ws, self.wps, xys)]),
-            sum([w * _f(xy[0], xy[1], xy[2])[0] * self.tri_element.N4(
+            sum([w * _f(xy[0], xy[1], xy[2]) * self.tri_element.N4(
                 wp[0], wp[1], wp[2]) for w, wp, xy in zip(self.ws, self.wps, xys)]),
             ])
 

examples/Heat/3D/heat_3D_ex001.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     """
     # Right-hand side function
     def RHS(_x, _y, _z):
-        return np.array([0., 0., 0.]) # gravity in N/m3
+        return 0.0
 
     """ 
     Create a mesh
@@ -49,8 +49,8 @@ if __name__ == '__main__':
     femmesh = FemMesh()
     exitCode = create_nodes(femmesh)
     exitCode = create_elements(femmesh)
-    simplices_coord = np.round(np.array(femmesh.faces, dtype=np.float32)[:, :], 2) / 100
-    mesh = Mesh_from_FreeCAD(_faces=simplices_coord, alpha=34) 
+    simplices_coord = np.round(np.array(femmesh.faces, dtype=np.float32)[:, :], 3) / 100
+    mesh = Mesh_from_FreeCAD(_faces=simplices_coord, alpha=1) 
     
     # Mesh data
     inner_points = [i for i in range(len(mesh.tri.points))
@@ -134,40 +134,43 @@ if __name__ == '__main__':
     grid_x, grid_y, grid_z = np.mgrid[x.min():x.max():100j, y.min():y.max():100j, z.min():z.max():100j]
     u_intp = sp.interpolate.griddata(mesh.tri.points, u, (grid_x, grid_y, grid_z), method='linear')
     lrange = np.linspace(u.min(), u.max(), 6)
-    label = r'$u$'
     cmap = cm.coolwarm
+    aspect= 'auto' # 'equal'
     
     plt.rcParams['figure.figsize'] = 16, 6
     fig = plt.figure()
     ax = fig.add_subplot(1, 3, 1)
-    surf = ax.contourf(grid_x[:, :, 50], grid_y[:, :, 50], u_intp[:, :, 50], cmap=cmap)
+    label = r'$u(z=L_z/4)$'
+    surf = ax.contourf(grid_x[:, :, 25], grid_y[:, :, 25], u_intp[:, :, 25], cmap=cmap)
     ax.set_xlim([x.min(), x.max()])
     ax.set_ylim([y.min(), y.max()])
     ax.set_xlabel(r'$x$')
     ax.set_ylabel(r'$y$')
-    ax.set_aspect('equal')
+    ax.set_aspect(aspect)
     cbar = plt.colorbar(surf, ax=ax, orientation="horizontal", pad=0.1, ticks=lrange)
     cbar.ax.set_xlabel(label)
     cbar.ax.xaxis.set_label_position('bottom')
     
     ax = fig.add_subplot(1, 3, 2)
-    surf = ax.contourf(grid_x[:, 50, :], grid_z[:, 50, :], u_intp[:, 50, :], cmap=cmap)
+    label = r'$u(z=L_z/2)$'
+    surf = ax.contourf(grid_x[:, :, 50], grid_y[:, :, 50], u_intp[:, :, 50], cmap=cmap)
     ax.set_xlim([x.min(), x.max()])
-    ax.set_ylim([z.min(), z.max()])
+    ax.set_ylim([y.min(), y.max()])
     ax.set_xlabel(r'$x$')
-    ax.set_ylabel(r'$z$')
-    ax.set_aspect('equal')
+    ax.set_ylabel(r'$y$')
+    ax.set_aspect(aspect)
     cbar = plt.colorbar(surf, ax=ax, orientation="horizontal", pad=0.1, ticks=lrange)
     cbar.ax.set_xlabel(label)
     cbar.ax.xaxis.set_label_position('bottom')
     
     ax = fig.add_subplot(1, 3, 3)
-    surf = ax.contourf(grid_y[50, :, :], grid_z[50, :, :], u_intp[50, :, :], cmap=cmap)
-    ax.set_xlim([y.min(), y.max()])
-    ax.set_ylim([z.min(), z.max()])
-    ax.set_xlabel(r'$y$')
-    ax.set_ylabel(r'$z$')
-    ax.set_aspect('equal')
+    label = r'$u(z=3L_z/4)$'
+    surf = ax.contourf(grid_x[:, :, 75], grid_y[:, :, 75], u_intp[:, :, 75], cmap=cmap)
+    ax.set_xlim([x.min(), x.max()])
+    ax.set_ylim([y.min(), y.max()])
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$y$')
+    ax.set_aspect(aspect)
     cbar = plt.colorbar(surf, ax=ax, orientation="horizontal", pad=0.1, ticks=lrange)
     cbar.ax.set_xlabel(label)
     cbar.ax.xaxis.set_label_position('bottom')

examples/Heat/3D/heat_3D_ex002.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     """
     # Right-hand side function
     def RHS(_x, _y, _z):
-        return np.array([0., 0., 0.])
+        return 0.0
 
     """ 
     Create a rectangular mesh
@@ -49,8 +49,8 @@ if __name__ == '__main__':
     femmesh = FemMesh()
     exitCode = create_nodes(femmesh)
     exitCode = create_elements(femmesh)
-    simplices_coord = np.round(np.array(femmesh.faces, dtype=np.float32)[:, :], 2) / 100
-    mesh = Mesh_from_FreeCAD(_faces=simplices_coord, alpha=34, outline=np.loadtxt('data/naca6412.dat'))
+    simplices_coord = np.round(np.array(femmesh.faces, dtype=np.float32)[:, :], 3) / 100
+    mesh = Mesh_from_FreeCAD(_faces=simplices_coord, alpha=1, outline=np.loadtxt('data/naca6412.dat'))
     # Mesh data
     inner_points = [i for i in range(len(mesh.tri.points))
                     if i not in mesh.boundary_points]
@@ -134,12 +134,13 @@ if __name__ == '__main__':
     xidx = 50
     yidx = 60
     zidx = 50
-    aspect='equal' # 'auto'
+    aspect= 'auto' # 'equal'
     
     plt.rcParams['figure.figsize'] = 16, 6
     fig = plt.figure()
     ax = fig.add_subplot(1, 3, 1)
-    surf = ax.contourf(grid_x[:, :, zidx], grid_y[:, :, zidx], u_intp[:, :, zidx], cmap=cmap)
+    label = r'$u(z=L_z/4)$'
+    surf = ax.contourf(grid_x[:, :, 25], grid_y[:, :, 25], u_intp[:, :, 25], cmap=cmap)
     ax.set_xlim([x.min(), x.max()])
     ax.set_ylim([y.min(), y.max()])
     ax.set_xlabel(r'$x$')
@@ -150,21 +151,23 @@ if __name__ == '__main__':
     cbar.ax.xaxis.set_label_position('bottom')
     
     ax = fig.add_subplot(1, 3, 2)
-    surf = ax.contourf(grid_x[:, yidx, :], grid_z[:, yidx, :], u_intp[:, yidx, :], cmap=cmap)
+    label = r'$u(z=L_z/2)$'
+    surf = ax.contourf(grid_x[:, :, 50], grid_y[:, :, 50], u_intp[:, :, 50], cmap=cmap)
     ax.set_xlim([x.min(), x.max()])
-    ax.set_ylim([z.min(), z.max()])
+    ax.set_ylim([y.min(), y.max()])
     ax.set_xlabel(r'$x$')
-    ax.set_ylabel(r'$z$')
+    ax.set_ylabel(r'$y$')
     ax.set_aspect(aspect)
     cbar = plt.colorbar(surf, ax=ax, orientation="horizontal", pad=0.1, ticks=lrange)
     cbar.ax.set_xlabel(label)
     cbar.ax.xaxis.set_label_position('bottom')
     
     ax = fig.add_subplot(1, 3, 3)
-    surf = ax.contourf(grid_z[xidx, :, :], grid_y[xidx, :, :], u_intp[xidx, :, :], cmap=cmap)
+    label = r'$u(z=3L_z/4)$'
+    surf = ax.contourf(grid_x[:, :, 75], grid_y[:, :, 75], u_intp[:, :, 75], cmap=cmap)
+    ax.set_xlim([x.min(), x.max()])
     ax.set_ylim([y.min(), y.max()])
-    ax.set_xlim([z.min(), z.max()])
-    ax.set_xlabel(r'$z$')
+    ax.set_xlabel(r'$x$')
     ax.set_ylabel(r'$y$')
     ax.set_aspect(aspect)
     cbar = plt.colorbar(surf, ax=ax, orientation="horizontal", pad=0.1, ticks=lrange)